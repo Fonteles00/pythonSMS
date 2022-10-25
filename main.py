7caf20fde346086ef56c8da20d5bdb9325c06f24
fonteles.davi <92239045+Fonteles00@users.noreply.github.com>
2022-10-24 22:43:21 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from twilio.rest import Client
+
+# Abrir os 6 arquivos em Excel
+
+# Para cada arquivo:
+
+# Verificar se algum valor na coluna Vendas daquele arquivo é maior que 55.000
+
+# Se for maior do que 55.000 -> Envia um SMS com o Nome, o mês e as vendas do vendedor
+
+# Caso não seja maior do que 55.000 não quero fazer nada
+
+# Your Account SID from twilio.com/console
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+# Your Auth Token from twilio.com/console
+auth_token  = "your_auth_token"
+client = Client(account_sid, auth_token)
+
+
+
+lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
+
+for mes in lista_meses:
+    tabela_vendas = pd.read_excel(f'{mes}.xlsx')
+    if (tabela_vendas['Vendas'] > 55000).any():
+        vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendedor'].values[0]
+        vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
+        print(f'No mês {mes} alguém bateu a meta. Vendedor: {vendedor}, Vendas: {vendas}')
+        message = client.messages.create(
+            to="+5521999999999",
+            from_="seu_numero_twillio",
+            body=f'No mês {mes} alguém bateu a meta. Vendedor: {vendedor}, Vendas: {vendas}')
+        print(message.sid)
